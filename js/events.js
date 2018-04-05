8fd591682d0b5f35e496191d21439a31e1d9ee46
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-04-05 22:28:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 
 function pressIt(){
   $("form").on("keydown", function(){
-    if ($( "input" ).val() === "G") {
+    if ($( "input:first" ).val() === "G") {
     alert('you pressed the g key');
     
   }
