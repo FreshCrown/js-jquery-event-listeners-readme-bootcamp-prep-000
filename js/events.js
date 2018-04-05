8660b70ff080e5aa9961821a5c0a1255618ce3bc
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-04-05 22:15:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,9 +23,9 @@ function pressIt(){
 function submitIt(){
   $("form").on("submit", function() {
      alert('your form is going to be submitted now')
-}
+  }
 });
-
+}
 
 
 $(document).ready(function(){
