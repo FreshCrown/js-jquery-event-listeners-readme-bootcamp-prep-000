b7c062c5738d128d9876382d7751e5264ab9dc8e
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-04-05 22:00:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,13 +6,13 @@ function getIt(){
 }
 
 function frameIt(){
-  $('img').on('load', function(){
+
     $('img').addClass('tasty')
   });
 }
 
 function pressIt(){
-  
+    $('img').on('load', function(){
 }
 
 $(document).ready(function(){
