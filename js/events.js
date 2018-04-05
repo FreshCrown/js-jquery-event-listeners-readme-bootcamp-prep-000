dbc6cdf13f812c467a4d999f2b81dfdb1b5e62b5
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-04-05 22:13:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,11 @@ function pressIt(){
 }
 
 function submitIt(){
-  
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now')
+}
+});
 }
 
 
